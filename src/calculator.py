048e91f04d9b5e878c3b5f040e2ef37e379e3ae8
Rodrigo <rodri@google.com>
2025-06-05 15:16:18 -0400

    test de los metodos suma, resta y multiplicar

--- src/calculator.py
+++ src/calculator.py
@@ -3,4 +3,6 @@ def suma(a, b):
 
 def resta(a, b):
     return a - b
-    
+
+def multiplicar(a, b):
+    return a * b
--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -1,13 +1,16 @@
 import unittest
 
-from src.calculator import suma, resta
+from src.calculator import suma, resta, multiplicar
 
 class CalculatorTest(unittest.TestCase):
-   
+
     def test_sum(self):
         self.assertEqual(suma(4.2, 3), 7.2)
 
     def test_resta(self):
         self.assertEqual(resta(4, 3), 1)
 
+
+    def test_multiplicar(self):
+        self.assertEqual(multiplicar(2, 3), 6)
 
